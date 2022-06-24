85cc9c0818d4de5237e03c26003490da9ac64e90
Albert <s95551@pollub.edu.pl>
2022-06-24 18:14:08 +0200

    Czwarty Commit

--- Klasa1.java
+++ Klasa1.java
@@ -1,9 +1,6 @@
 public class Klasa1 {
     public static void main(String[] args) {
-        System.out.println("Commit 2");
-        System.out.println("Commit 2");
-        System.out.println("Commit 2");
-        System.out.println("Commit 2");
-        System.out.println("Commit 2");
+        System.out.println("Commit 4");
+
     }
 }
